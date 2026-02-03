94f1ef32cbed87f4f5231c19993acbb6c3a745cd
AliyahGE <agutierrez@institutmvm.cat>
2026-02-03 18:41:13 +0100

    Exercici_1

---
+++ models.js
@@ -0,0 +1,21 @@
+class Multimedia {
+    #views = 0;
+
+    constructor(titol, durada) {
+        this.titol = titol;
+        this.durada = durada;
+    }
+
+    play() {
+        console.log("Reproduint...");
+        this.#views += 1;
+    }
+
+    get_views() {
+        return this.#views;
+    }
+    
+    #calcularCostServer() {
+       return this.durada * 0.01;
+    }
+}

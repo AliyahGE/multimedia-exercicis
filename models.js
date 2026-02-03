0f943945d339919943adde9052b76789e67ec1af
AliyahGE <agutierrez@institutmvm.cat>
2026-02-03 18:44:33 +0100

    Exercici_2

--- models.js
+++ models.js
@@ -19,3 +19,32 @@ class Multimedia {
        return this.durada * 0.01;
     }
 }
+class Peli extends Multimedia {
+
+    constructor(titol, durada, any, costFix) {
+        super(titol, durada);
+        this.any = any;
+        this.costFix = costFix;
+    }
+    
+    calcularRoyalties(){
+        return this.get_views * this.costFix;
+    }
+}
+
+class Serie extends Multimedia {
+
+    constructor(titol, durada, nombre_episodis) {
+        super(titol, durada);
+        this.nombre_episodis = nombre_episodis;
+    }
+
+    play() {
+        super.play();
+        return "Marató de" + this.titol + "iniciada";
+    }
+
+    calcularRoyalties() {
+        return this.get_views() * this.nombre_episodis * 0.05;
+    }
+}     
